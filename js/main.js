68c350426ffe6d2b649aa150d03b920a7cfbd128
Nathan Jumns <isjonathaniel@gmail.com>
2020-11-10 13:50:43 +0100

    modified the animation delay

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 // All animations will take twice the time to accomplish
 document.documentElement.style.setProperty("--animate-duration", "2s");
+
+AOS.init({
+  duration: 1200,
+  animatedClassName: "aos-animate",
+  once: true,
+  initClassName: "aos-init",
+  easing: " linear",
+});
