54c416d96fef5dc37c421853d8b5ac9029be8e4f
Nathan Jumns <isjonathaniel@gmail.com>
2020-11-16 21:01:44 +0100

    modified animations

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 // All animations will take twice the time to accomplish
 document.documentElement.style.setProperty("--animate-duration", "2s");
 
-AOS.init({
-  duration: 1200,
-  animatedClassName: "aos-animate",
-  once: true,
-  initClassName: "aos-init",
-  easing: " linear",
+
+
+$(function () {
+  AOS.init({
+    duration: 1200,
+    animatedClassName: "aos-animate",
+    once: true,
+    initClassName: "aos-init",
+    easing: " linear",
+  });
 });
